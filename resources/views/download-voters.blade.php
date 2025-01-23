8789392f6c87cf7c57a1459700ba5dec753c5474
louie chiong <louiechiong@gmail.com>
2025-01-23 23:10:01 +0800

    add signature field

--- resources/views/download-voters.blade.php
+++ resources/views/download-voters.blade.php
@@ -127,6 +127,18 @@
                 </tr>
             </tfoot>
         </table>
+
+        <div style="margin-top: 20px">
+            <p>Confirme:</p>
+            <p style="text-align: left;">
+                <span style="border-bottom:2px solid">{{ $leader->full_name }}</span>
+            </p>
+            <p style="text-align: left; position: relative; width: 40%;">
+                <span style="text-align: center; position: absolute; top: 18px; left:74px">Signature</span>
+                <br />
+                <span>__________________________</span>
+            </p>
+        </div>
     </div>
 </body>
 </html>
